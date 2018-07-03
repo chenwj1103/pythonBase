cdb4a6ff72112696dbcf7ab27209a6606f6a8fb8
chenweijie <chenweijie@sunlands.com>
2018-07-03 19:50:05 +0800

    python基本数据类型学习

--- base/dictronaryDemo.py
+++ base/dictronaryDemo.py
@@ -1,15 +1,55 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
-dict ={}
+dict = {}
 dict['one'] = "this is one"
 dict[2] = "this is two"
 
-tinydict ={'name':'john','code':'123','dept':'sales'}
+tinydict = {'name': 'john', 'code': '123', 'dept': 'sales'}
 
-print dict['one']
+print "dict['one']:", dict['one']
 print dict[2]
 print tinydict
 print dict
-print tinydict.viewkeys()
-print tinydict.viewvalues()
+print tinydict.items()
+print tinydict.keys()
+
+# 删除字典字符串
+del dict[2]
+print "after del:", dict
+
+tinydict = {'name': 'john', 'code': '123', 'dept': 'sales'}
+tinySet = {'a', 6, 7}
+tinyList = ['f', 7, 9]
+tinyTup = (6, 8, 9)
+print 'tinydict type:', type(tinydict)
+print 'tinySet type:', type(tinySet)
+print 'tinyList type:', type(tinyList)
+print 'tinyTup type:', type(tinyTup)
+
+print "after del:", dict
+dict.clear() # 删除字典的所有元素
+print "after clear:", dict
+
+# 直接引用
+tinydict1 = tinydict
+
+# 浅copy 深拷贝父对象（一级目录），子对象（二级目录）不拷贝，还是引用
+tinydict2 = tinydict.copy()
+tinydict['name'] = '张三'
+del tinydict['name']
+
+print 'tinydict1：', tinydict1
+print 'tinydict2：', tinydict2
+
+tinydict2.fromkeys()
+tinydict2.get(tinydict['name'], "")
+tinydict2.has_key(tinydict['name'])
+tinydict2.items()
+tinydict2.keys()
+tinydict2.setdefault(tinydict['name'], "")
+tinydict2.update(tinydict)
+tinydict2.values()
+tinydict2.pop(tinydict['name'])
+tinydict2.popitem()
 
---
+++ operator/TupTest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 定义元组
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (3, 6, 7, 8, 9)
+
+print 'tup1:', tup1
+print 'tup2:', tup2
+
+# 访问元组
+print 'tup1[0]', tup1[0]
