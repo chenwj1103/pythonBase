baad7f83a1197e1ac84254e08a89dabbf1f3bc72
chenwj <cwj11030919@gmail.com>
2018-07-11 00:29:07 +0800

    update

--- base/listDemo.py
+++ base/listDemo.py
@@ -35,37 +35,10 @@ print len(lenList)
 # 获取列表元素的最大值
 print max(lenList)
 # 将元祖转化为列表
-tup = (5, 7, 9, 0)
+tup = (5,7,9,0)
 print tup
 tupList = list(tup)
 print tupList
+
 # 添加元素
-tupList.append(5)
-# 统计某个元素在列表中出现的次数
-n = tupList.count(5)
-print '元素在列表中出现的次数：', n
-
-# 一次性扩展多个值
-aList = [123, 'xyz', 'zara', 'abc', 123]
-bList = [2009, 'manni']
-aList.extend(bList)
-
-# 列出某个元素在列表中的位置
-bList.index(123)
-
-# 在某个位置插入某个元素
-list.insert(3, 'dddd')
-
-# 移除列表中的一个元素（默认最后一个元素），并且返回该元素的值
-bList.pop(-1)
-
-# 移除列表中某个值的第一个匹配项
-bList.remove(5)
-
-'''
-cmp -- 可选参数, 如果指定了该参数会使用该参数的方法进行排序。
-key -- 主要是用来进行比较的元素，只有一个参数，具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序。
-reverse -- 排序规则，reverse = True 降序， reverse = False 升序（默认）。
-'''
-# 排序
-list.sort(cmp=None, key=None, reverse=False)
+dDist.append(5)
