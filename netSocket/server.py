c708da4ce6e26875df95f65ba86588230e6c494e
chenwj <cwj11030919@gmail.com>
2018-09-11 00:34:10 +0800

    添加网络变成和多线程的例子

---
+++ netSocket/server.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+# 服务端
+
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host, port))
+
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print "连接地址：", addr
+    c.send('欢迎访问菜鸟教程！')
+    c.close()
